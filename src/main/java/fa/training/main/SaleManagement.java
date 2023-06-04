96cdaa82e17ed92274db05a7e3b846440205985b
CanhNT <canhnguyenthanhnt@gmail.com>
2023-06-04 13:55:18 +0700

    add file in DAO and common

--- src/main/java/fa/training/main/SaleManagement.java
+++ src/main/java/fa/training/main/SaleManagement.java
@@ -1,9 +1,21 @@
 package fa.training.main;
 
-import fa.training.common.Database;
+
+import fa.training.dao.CustomerDAO;
+import fa.training.entities.Customer;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class SaleManagement {
-    public static void main(String[] args) {
-        Database database = new Database();
+    public static void main(String[] args) throws SQLException {
+        CustomerDAO customerDAO = new CustomerDAO();
+
+        int count = customerDAO.delete(1);
+        System.out.println(count);
+        List<Customer> list = customerDAO.retrieve();
+        for (Customer c : list) {
+            System.out.println(c);
+        }
     }
 }
